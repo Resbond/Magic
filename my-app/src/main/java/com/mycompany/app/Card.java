49ebbcfb89ec06a4d3a9a86c454f6d01711a3747
Pepe <jose_ascencioe@hotmail.com>
2023-04-11 13:56:43 -0500

    avanzando

---
+++ my-app/src/main/java/com/mycompany/app/Card.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+public class Card {
+    String name;
+    int cost;
+    String cardType;
+    String effect;
+    int expansion;
+    
+}
---
+++ my-app/src/main/java/com/mycompany/app/CardTest.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class CardTest {
+    
+    public static void main(String[] args) {
+        Card firstCard = new Card();
+    }
+}
